43e08bcfb8c3ecd215d8a0d6296e3551ecd154f9
Evie-Skinner18 <eskinner@spartaglobal.com>
2018-12-16 11:42:19 +0000

    Removed unecessary require and include statements from music step defs

--- features/lib/pages/the_uplifter_home.rb
+++ features/lib/pages/the_uplifter_home.rb
@@ -1,15 +1,15 @@
 # https://www.theuplifter.co.uk/
 
-# require 'capybara/dsl'
-
 # This is the POM for the homepage
 class Homepage
 #include the module so we can now run nice Capybara methods straigth away
   include Capybara::DSL
 
+# each instance of the Homepage object will automatically have all these properties
   def initialize
     @homepage_url = 'https://www.theuplifter.co.uk/'
     @homepage_button = 'Home'
+    @
 
   end
 
--- features/step_definitions/the_uplifter_music_step_defs.rb
+++ features/step_definitions/the_uplifter_music_step_defs.rb
@@ -1,13 +1,13 @@
-require_relative '../lib/uplifter_site'
-include TheUplifterSite
+# no need to require the superclass for site as it's a module so available everywhere
 
 # homepage: flag so high
 Given('that I can open the website') do
   the_uplifter_home.visit_homepage
 end
 
-Given('I can navigate to the homepage') do
-  pending # Write code here that turns the phrase above into concrete actions
+And('I can navigate to the homepage') do
+  Pending
+  #the_uplifter_home.
 end
 
 When('I click on the play button for Flag So High') do
